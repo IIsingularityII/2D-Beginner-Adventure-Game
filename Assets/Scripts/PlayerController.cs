b9ab2d58197f8a6008a8dda7e42add4dbb5aa254
SfarxX <alupachiov@mail.ru>
2024-07-24 18:43:16 +0300

    Implemented physics in movement, colliders, configured tilemap, used Composite Collider

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,24 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    private Rigidbody2D _rigidbody2d;
+    private Vector2 move;
     private float _playerSpeed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        _rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * _playerSpeed * Time.deltaTime;  
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 position = (Vector2)_rigidbody2d.position + move * _playerSpeed * Time.deltaTime;
+        _rigidbody2d.MovePosition(position);
     }
 }
